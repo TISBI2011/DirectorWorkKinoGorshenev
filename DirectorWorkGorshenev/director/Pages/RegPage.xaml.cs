0040fada8d07dafe9d3eedf34873656e034f7e94
ИС201-1 <IS201-1@tisbi.ru>
2023-12-12 17:11:23 +0300

    gs

---
+++ DirectorWorkGorshenev/director/Pages/RegPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using director.Moduls;
+
+namespace director.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для RegPage.xaml
+    /// </summary>
+    public partial class RegPage : Page
+    {
+        User contextUser;
+        public RegPage(User user)
+        {
+            InitializeComponent();
+            contextUser = user;
+            DataContext = contextUser;
+        }
+
+        private void BRegister_Click(object sender, RoutedEventArgs e)
+        {
+            if (contextUser.Id == 0)
+                App.DB.User.Add(contextUser);
+            App.DB.SaveChanges();
+            NavigationService.GoBack();
+
+
+           
+        }
+
+
+
+        
+    }
+}
